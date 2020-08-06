63399a34da9f297df19bcf22be4bf2f732974c57
Isaac Gorman <emailme@isaacg.net>
2020-08-06 15:19:41 -0500

    test test passed, on to input testing

---
+++ src/scratchPaper.js
@@ -0,0 +1,6 @@
+// What do I want to test?
+
+// 1) Each input
+// 2) Why a "Looks like there was an error: maxLength" is being displayed to the user
+// 3) That I can submit the info submitted into the input fields
+// ... that it for now
